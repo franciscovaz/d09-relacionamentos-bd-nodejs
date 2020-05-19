dc580a7b1c103cc9ac3b8cf2dc92fd7ff8fca718
Francisco Vaz <ftomasvaz@gmail.com>
2020-05-19 22:41:26 +0100

    orders and products repository

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -6,20 +6,31 @@ import {
   OneToOne,
   JoinColumn,
   OneToMany,
+  Column,
 } from 'typeorm';
 
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
+// import CreateCustomerService from '@modules/customers/services/CreateCustomerService';
 
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
+  customer_id: string;
+
+  @OneToMany(() => Customer, customer => customer.id)
+  @JoinColumn({ name: 'customer_id' })
   customer: Customer;
 
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
